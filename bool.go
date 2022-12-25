46394ec8e9dc2494984bbdad9d723c302e29c0d0
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2022-12-25 14:24:34 +0900

    Add BoolTest

---
+++ bool.go
@@ -0,0 +1,29 @@
+package gt
+
+import "testing"
+
+type BoolTest struct {
+	t      testing.TB
+	actual bool
+}
+
+func Bool(t *testing.T, actual bool) BoolTest {
+	return BoolTest{
+		t:      t,
+		actual: actual,
+	}
+}
+
+func (x BoolTest) True() BoolTest {
+	if !x.actual {
+		x.t.Error("expected true, but false")
+	}
+	return x
+}
+
+func (x BoolTest) False() BoolTest {
+	if x.actual {
+		x.t.Error("expected false, but true")
+	}
+	return x
+}
