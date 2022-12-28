51834d8f5f18b753a5cc87e6b23c45a55f72a418
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2022-12-28 10:16:56 +0900

    Add comments

--- cast.go
+++ cast.go
@@ -2,6 +2,11 @@ package gt
 
 import "testing"
 
+// Cast tries type assertion and will error if type assertion fails
+//
+//	var a any = "hello"
+//	b := gt.Cast[string](a) // <- Pass and set "hello" to b
+//	c := gt.Cast[int](a)    // <- Fail and set empty to c
 func Cast[T any](t testing.TB, v any) T {
 	casted, ok := v.(T)
 	if !ok {
@@ -11,6 +16,11 @@ func Cast[T any](t testing.TB, v any) T {
 	return casted
 }
 
+// MustCast tries type assertion and will error and stop test if type assertion fails
+//
+//	var a any = "hello"
+//	b := gt.MustCast[string](a) // <- Pass and set "hello" to b
+//	c := gt.MustCast[int](a)    // <- Fail and stop test
 func MustCast[T any](t testing.TB, v any) T {
 	casted, ok := v.(T)
 	if !ok {
