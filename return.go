e14f99a61f50f8b47f263b3083afc7a0e82616f0
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2023-01-14 11:16:25 +0900

    Add syntax sugar for ReturnN

--- return.go
+++ return.go
@@ -14,6 +14,10 @@ func Return1[T1 any](r1 T1, err error) Return1Test[T1] {
 	}
 }
 
+func R1[T1 any](r1 T1, err error) Return1Test[T1] {
+	return Return1(r1, err)
+}
+
 func (x Return1Test[T1]) Error(t testing.TB) {
 	t.Helper()
 	if x.err == nil {
@@ -42,6 +46,10 @@ func Return2[T1, T2 any](r1 T1, r2 T2, err error) Return2Test[T1, T2] {
 		r2:  r2,
 		err: err,
 	}
+}
+
+func R2[T1, T2 any](r1 T1, r2 T2, err error) Return2Test[T1, T2] {
+	return Return2(r1, r2, err)
 }
 
 func (x Return2Test[T1, T2]) Error(t testing.TB) {
