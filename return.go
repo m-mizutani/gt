54b86ca8d904ea70bd016c7bfe65fe0436e3adbc
Masayoshi Mizutani <masayoshi.mizutani@dr-ubie.com>
2023-01-15 09:31:40 +0900

    Add comments

--- return.go
+++ return.go
@@ -7,6 +7,13 @@ type Return1Test[T1 any] struct {
 	err error
 }
 
+// Return1 creates a test for returned variables (one value and one error)
+//
+//	f := func() (string, error) {
+//		return "ok", nil
+//	}
+//	gt.Return1(f()).Error()               // Fail
+//	gt.Return1(f()).NoError().Equal("ok") // Pass
 func Return1[T1 any](r1 T1, err error) Return1Test[T1] {
 	return Return1Test[T1]{
 		r1:  r1,
@@ -18,13 +25,16 @@ func R1[T1 any](r1 T1, err error) Return1Test[T1] {
 	return Return1(r1, err)
 }
 
-func (x Return1Test[T1]) Error(t testing.TB) {
+// Error check if the function returned error. If error is nil, it will fail. If error is not nil, it provides ErrorTest
+func (x Return1Test[T1]) Error(t testing.TB) ErrorTest {
 	t.Helper()
 	if x.err == nil {
 		t.Errorf("got no error, but should get errored")
 	}
+	return Error(t, x.err)
 }
 
+// NoError check if the function returned no error. If error is not nil, it will fail. If error is nil, it provides ValueTest for 1st returned value.
 func (x Return1Test[T1]) NoError(t testing.TB) ValueTest[T1] {
 	t.Helper()
 	if x.err != nil {
@@ -40,6 +50,16 @@ type Return2Test[T1, T2 any] struct {
 	err error
 }
 
+// Return2 creates a test for returned variables (two values and one error)
+//
+//	f := func() (string, int, error) {
+//		return "ok", 1, nil
+//	}
+//	gt.Return2(f()).Error()           // Fail
+//	s, i := gt.Return1(f()).NoError() // Pass
+//
+//	s.Equal("ok") // Pass
+//	i.Equal(1)    // Pass
 func Return2[T1, T2 any](r1 T1, r2 T2, err error) Return2Test[T1, T2] {
 	return Return2Test[T1, T2]{
 		r1:  r1,
@@ -52,14 +72,67 @@ func R2[T1, T2 any](r1 T1, r2 T2, err error) Return2Test[T1, T2] {
 	return Return2(r1, r2, err)
 }
 
-func (x Return2Test[T1, T2]) Error(t testing.TB) {
+// Error check if the function returned error. If error is nil, it will fail. If error is not nil, it provides ErrorTest
+func (x Return2Test[T1, T2]) Error(t testing.TB) ErrorTest {
 	t.Helper()
 	if x.err == nil {
 		t.Errorf("got no error, but should get errored")
 	}
+	return Error(t, x.err)
 }
 
+// NoError check if the function returned no error. If error is not nil, it will fail. If error is nil, it provides ValueTest for 1st and 2nd returned value.
 func (x Return2Test[T1, T2]) NoError(t testing.TB) (ValueTest[T1], ValueTest[T2]) {
+	t.Helper()
+	if x.err != nil {
+		t.Errorf("got errored, but should not get error")
+	}
+
+	return Value(t, x.r1), Value(t, x.r2)
+}
+
+type Return3Test[T1, T2, T3 any] struct {
+	r1  T1
+	r2  T2
+	r3  T3
+	err error
+}
+
+// Return3 creates a test for returned variables (three values and one error)
+//
+//	f := func() (string, int, bool, error) {
+//		return "ok", 1, true, nil
+//	}
+//	gt.Return3(f()).Error()           // Fail
+//	s, i, b := gt.Return1(f()).NoError() // Pass
+//
+//	s.Equal("ok") // Pass
+//	i.Equal(1)    // Pass
+//	b.Equal(true) // Pass
+func Return3[T1, T2, T3 any](r1 T1, r2 T2, r3 T3, err error) Return3Test[T1, T2, T3] {
+	return Return3Test[T1, T2, T3]{
+		r1:  r1,
+		r2:  r2,
+		r3:  r3,
+		err: err,
+	}
+}
+
+func R3[T1, T2, T3 any](r1 T1, r2 T2, r3 T3, err error) Return3Test[T1, T2, T3] {
+	return Return3(r1, r2, r3, err)
+}
+
+// Error check if the function returned error. If error is nil, it will fail. If error is not nil, it provides ErrorTest
+func (x Return3Test[T1, T2, T3]) Error(t testing.TB) ErrorTest {
+	t.Helper()
+	if x.err == nil {
+		t.Errorf("got no error, but should get errored")
+	}
+	return Error(t, x.err)
+}
+
+// NoError check if the function returned no error. If error is not nil, it will fail. If error is nil, it provides ValueTest for 1st, 2nd and 3rd returned value.
+func (x Return3Test[T1, T2, T3]) NoError(t testing.TB) (ValueTest[T1], ValueTest[T2]) {
 	t.Helper()
 	if x.err != nil {
 		t.Errorf("got errored, but should not get error")
